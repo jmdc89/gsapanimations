0278b2eded8448aa2033511d4c7aa5f56e26f2ed
Jose Manuel <jmdcarq89@gmail.com>
2021-03-12 09:56:54 +0100

    imgRev useEffect

--- src/components/ImageReveal.js
+++ src/components/ImageReveal.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import "./ImageReveal.scss";
 import People from "../images/vadim-sadovski--Q9YUzjHuis-1440.webp";
+import CSSRulePlugin from "gsap/CSSRulePlugin";
+import {TimelineLite, Power2} from "gsap";
 
 const ImageReveal = () => {
+
+    let container = useRef(null)
+    let image = useRef(null)
+    let imageReveal = CSSRulePlugin.getRule(".img-container:after");
+
+    const tl = new TimelineLite()
+
+    useEffect(() => {
+        tl.to(container, 1, {css: {visibility: 'visible'}}).to(imageReveal, 1.4, {width: "0%", ease:Power2.easeInOut}).from(image, 1.4, {scale: 1.6, ease: Power2.easeInOut, delay: -1.5})
+    })
+
     return (
         <section className="main">
-            <div className="container">
+            <div
+            ref={el => container = el} 
+            className="container">
                 <>
                     <div className="img-container">
-                        <img src={People}/>
+                        <img
+                        ref={el => image = el} 
+                        src={People}/>
                     </div>
                 </>
             </div>
