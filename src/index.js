39f74d7f594d38163987801712c225a2329a3754
Jose Manuel <jmdcarq89@gmail.com>
2021-03-12 20:30:09 +0100

    image fix

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Circles from './components/Circles';
 import ImageReveal from './components/ImageReveal';
+import Slider from './components/Slider';
 
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <Circles /> */}
     {/* <ImageReveal/> */}
+    <Slider/>
   </React.StrictMode>,
   document.getElementById('root')
 );
